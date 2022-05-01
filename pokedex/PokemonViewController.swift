f84d71d3922454bad30278dee3ead17795db3cf5
Kyleg97 <kyle.gilbert1622@gmail.com>
2022-04-30 20:16:50 -0700

    add more labels for stats

--- pokedex/PokemonViewController.swift
+++ pokedex/PokemonViewController.swift
@@ -12,6 +12,19 @@ class PokemonViewController: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var entryNumLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var hpLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var specialAttackLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var specialDefenseLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
     @IBOutlet weak var pokemonImage: UIImageView!
     
     let networking = Networking()
@@ -57,6 +70,25 @@ class PokemonViewController: UIViewController {
                     print(pokemon)
                     fetchImage()
                     entryNumLabel.text = "#\(pokemon!.order!)"
+                    
+                    heightLabel.text = "Height: \(decimetersToInches(height: pokemon!.height!)) inches"
+                    weightLabel.text = "Weight: \(hectogramsToLbs(weight: pokemon!.weight!)) lbs"
+                    
+                    hpLabel.text = "HP: \(pokemon!.stats![0].baseStat!)"
+                    attackLabel.text = "ATK: \(pokemon!.stats![1].baseStat!)"
+                    defenseLabel.text = "DEF: \(pokemon!.stats![2].baseStat!)"
+                    specialAttackLabel.text = "S.ATK: \(pokemon!.stats![3].baseStat!)"
+                    specialDefenseLabel.text = "S.DEF: \(pokemon!.stats![4].baseStat!)"
+                    speedLabel.text = "SPD: \(pokemon!.stats![5].baseStat!)"
+                    var typeString = ""
+                    let types = pokemon?.types!
+                    for i in 0...types!.count-1 {
+                        typeString += (types![i].type?.name)!
+                        if (i == 0) {
+                            typeString += " "
+                        }
+                    }
+                    typeLabel.text = typeString
                 }
                 // now we need to map value of this ^ list into the table somehow, check muni app
             }
